d011c9a367883127a60ca5a40465c058af26b013
myp015 <myp015@163.com>
2013-04-21 14:26:49 +0800

    finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  
+  attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
@@ -10,5 +10,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
